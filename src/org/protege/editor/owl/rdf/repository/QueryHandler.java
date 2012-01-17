8c7ed282f65d7ed967fd2f2199f93e5bb99e1b12
Timothy Redmond <tredmond@stanford.edu>
2012-01-17 16:02:27 +0000

    the reasoner inserts IRI's and class expressions into the result when it can

--- src/org/protege/editor/owl/rdf/repository/QueryHandler.java
+++ src/org/protege/editor/owl/rdf/repository/QueryHandler.java
@@ -3,14 +3,27 @@ package org.protege.editor.owl.rdf.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openrdf.model.BNode;
 import org.openrdf.model.Value;
 import org.openrdf.query.BindingSet;
 import org.openrdf.query.TupleQueryResultHandler;
 import org.openrdf.query.TupleQueryResultHandlerException;
+import org.openrdf.repository.RepositoryException;
 import org.protege.editor.owl.rdf.SparqlResultSet;
+import org.protege.owl.rdf.api.OwlTripleStore;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLDataFactory;
 
 public class QueryHandler implements TupleQueryResultHandler {
+	private OwlTripleStore triples;
+	private OWLDataFactory factory;
 	private SparqlResultSet queryResult;
+	
+	public QueryHandler(OwlTripleStore triples, OWLDataFactory factory) {
+		this.triples = triples;
+		this.factory = factory;
+	}
 	
 	public SparqlResultSet getQueryResult() {
 		return queryResult;
@@ -23,13 +36,32 @@ public class QueryHandler implements TupleQueryResultHandler {
 
 	@Override
 	public void handleSolution(BindingSet bindingSet) throws TupleQueryResultHandlerException {
+		try {
 		List<Object> row = new ArrayList<Object>();
 		for (int i = 0; i < queryResult.getColumnCount(); i++) {
 			String columnName = queryResult.getColumnName(i);
 			Value v = bindingSet.getBinding(columnName).getValue();
-			row.add(v);
+			row.add(convertValue(v));
 		}
 		queryResult.addRow(row);
+		}
+		catch (RepositoryException re) {
+			throw new TupleQueryResultHandlerException(re);
+		}
+	}
+	
+	private Object convertValue(Value v) throws RepositoryException {
+		Object converted = v;
+		if (v instanceof BNode) {
+			OWLClassExpression ce = triples.parseClassExpression((BNode) v);
+			if (ce != null) {
+				converted = ce;
+			}
+		}
+		else if (v instanceof org.openrdf.model.URI) {
+			converted = IRI.create(((org.openrdf.model.URI) v).stringValue());
+		}
+		return converted;
 	}
 
 	@Override
