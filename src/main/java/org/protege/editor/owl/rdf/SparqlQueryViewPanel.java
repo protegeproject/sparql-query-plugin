7443d87d0ecaa06070f48715bc280aa870741ecd
dfsantamaria <df.santamaria87@gmail.com>
2017-08-17 12:04:38 +0200

    Starting syntax highlighter

--- src/main/java/org/protege/editor/owl/rdf/SparqlQueryViewPanel.java
+++ src/main/java/org/protege/editor/owl/rdf/SparqlQueryViewPanel.java
@@ -57,8 +57,11 @@ import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
 import javax.swing.text.DocumentFilter;
 import javax.swing.text.PlainDocument;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
 import org.protege.editor.owl.rdf.repository.BasicSparqlReasonerFactory;
 
 /*
@@ -797,6 +800,79 @@ public class OptionDialog extends JDialog
   }
 
 
+     /*
+         Thanks to @shuangwhywhy for this class
+      */
+   class SPARQLQueryDocument extends DefaultStyledDocument
+     {
+        StyleContext cont;
+        AttributeSet attr;
+        AttributeSet attrBlack;
+        public SPARQLQueryDocument()
+          {
+            super();
+            cont = StyleContext.getDefaultStyleContext();
+            attr = cont.addAttribute(cont.getEmptySet(), StyleConstants.Foreground, Color.RED);
+            attrBlack = cont.addAttribute(cont.getEmptySet(), StyleConstants.Foreground, Color.BLACK);
+          }
+        @Override
+        public void insertString (int offset, String str, AttributeSet a) throws BadLocationException 
+          {
+            super.insertString(offset, str, a);
+            String text = getText(0, getLength());
+            int before = findLastNonWordChar(text, offset);
+            if (before < 0) 
+                before = 0;
+            int after = findFirstNonWordChar(text, offset + str.length());
+            int wordL = before;
+            int wordR = before;
+            while (wordR <= after) 
+              {
+               if (wordR == after || String.valueOf(text.charAt(wordR)).matches("\\W")) {
+                  if (text.substring(wordL, wordR).matches("(\\W)*(PREFIX|DISTINCT|SELECT|WHERE)"))
+                       setCharacterAttributes(wordL, wordR - wordL, attr, false);
+                   else
+                       setCharacterAttributes(wordL, wordR - wordL, attrBlack, false);
+                        wordL = wordR;
+               }
+                 wordR++;
+              }
+            }
+
+        @Override
+            public void remove (int offs, int len) throws BadLocationException {
+                super.remove(offs, len);
+
+                String text = getText(0, getLength());
+                int before = findLastNonWordChar(text, offs);
+                if (before < 0) before = 0;
+                int after = findFirstNonWordChar(text, offs);
+
+                if (text.substring(before, after).matches("(\\W)*(private|public|protected)")) {
+                    setCharacterAttributes(before, after - before, attr, false);
+                } else {
+                    setCharacterAttributes(before, after - before, attrBlack, false);
+                }
+            }
+            
+            private int findLastNonWordChar (String text, int index) {
+        while (--index >= 0) {
+            if (String.valueOf(text.charAt(index)).matches("\\W")) {
+                break;
+            }
+        }
+        return index;
+    }
+    private int findFirstNonWordChar (String text, int index) {
+        while (index < text.length()) {
+            if (String.valueOf(text.charAt(index)).matches("\\W")) {
+                break;
+            }
+            index++;
+        }
+        return index;
+    }
+        }
 }
 
  class SPARQLResultTableModel extends DefaultTableModel 
