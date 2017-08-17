822d026344f652db34cd760c10c286dee53fd347
dfsantamaria <df.santamaria87@gmail.com>
2017-08-17 21:01:09 +0200

    Revert Commit to original

--- src/main/java/org/protege/editor/owl/rdf/SparqlQueryViewPanel.java
+++ src/main/java/org/protege/editor/owl/rdf/SparqlQueryViewPanel.java
@@ -800,7 +800,90 @@ public class OptionDialog extends JDialog
                  parent.repaint();
                }
       }
-  }   
+  }
+
+
+     /*
+         Thanks to @shuangwhywhy for this class
+      */
+   class SPARQLQueryDocument extends DefaultStyledDocument
+     {
+        StyleContext cont;
+        AttributeSet attr;
+        AttributeSet attrRed;
+        AttributeSet attrBlack;
+        AttributeSet attrGreen;
+        public SPARQLQueryDocument()
+          {
+            super();
+            cont = StyleContext.getDefaultStyleContext();
+            attr = cont.addAttribute(cont.getEmptySet(), StyleConstants.Foreground, Color.blue);
+            attrRed = cont.addAttribute(cont.getEmptySet(), StyleConstants.Foreground, Color.red);
+            attrBlack = cont.addAttribute(cont.getEmptySet(), StyleConstants.Foreground, Color.BLACK);
+            attrGreen = cont.addAttribute(cont.getEmptySet(), StyleConstants.Foreground, new Color(0,102,0));
+          }
+        @Override
+        public void insertString (int offset, String str, AttributeSet a) throws BadLocationException 
+          {
+            super.insertString(offset, str, a);
+            String text = getText(0, getLength());
+            int before = findLastNonWordChar(text, offset);
+            if (before < 0) 
+                before = 0;
+            int after = findFirstNonWordChar(text, offset + str.length());
+            int wordL = before;
+            int wordR = before;
+            while (wordR <= after) 
+              {
+               if (wordR == after || String.valueOf(text.charAt(wordR)).matches("\\W")) {
+                  if (text.substring(wordL, wordR).matches("(\\W)*(PREFIX|DISTINCT|SELECT|WHERE)"))
+                       setCharacterAttributes(wordL, wordR - wordL, attr, false);
+                  else if (text.substring(wordL, wordR).matches("(\\W)*(\\{|\\}|\\<|>)"))
+                       setCharacterAttributes(wordL, wordR - wordL, attrRed, false);
+                  else if (text.substring(wordL, wordR).matches("(\\W)*\\?[\\w]+"))
+                       setCharacterAttributes(wordL, wordR - wordL, attrGreen, false);
+                   else
+                       setCharacterAttributes(wordL, wordR - wordL, attrBlack, false);
+                        wordL = wordR;
+               }
+                 wordR++;
+              }
+            }
+
+        @Override
+            public void remove (int offs, int len) throws BadLocationException {
+                super.remove(offs, len);
+
+                String text = getText(0, getLength());
+                int before = findLastNonWordChar(text, offs);
+                if (before < 0) before = 0;
+                int after = findFirstNonWordChar(text, offs);
+
+                if (text.substring(before, after).matches("(\\W)*(private|public|protected)")) {
+                    setCharacterAttributes(before, after - before, attr, false);
+                } else {
+                    setCharacterAttributes(before, after - before, attrBlack, false);
+                }
+            }
+            
+            private int findLastNonWordChar (String text, int index) {
+        while (--index >= 0) {
+            if (String.valueOf(text.charAt(index)).matches("\\W")) {
+                break;
+            }
+        }
+        return index;
+    }
+    private int findFirstNonWordChar (String text, int index) {
+        while (index < text.length()) {
+            if (String.valueOf(text.charAt(index)).matches("\\W")) {
+                break;
+            }
+            index++;
+        }
+        return index;
+    }
+        }
 }
 
  class SPARQLResultTableModel extends DefaultTableModel 
