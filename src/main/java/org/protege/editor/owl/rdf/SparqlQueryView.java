189fbd0e044ad9a4d228931258ea563c0d67955e
Matthew Horridge <matthew.horridge@stanford.edu>
2018-12-17 17:16:20 -0800

    Tidied code

--- src/main/java/org/protege/editor/owl/rdf/SparqlQueryView.java
+++ src/main/java/org/protege/editor/owl/rdf/SparqlQueryView.java
@@ -8,21 +8,19 @@ import org.protege.editor.owl.ui.view.AbstractOWLViewComponent;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Collections;
 import java.util.List;
 
 public class SparqlQueryView extends AbstractOWLViewComponent {
-	private static final long serialVersionUID = -1370725700740073290L;
-	
+
 	private SparqlReasoner reasoner;
-	private JTextPane queryPane;
-	private JButton executeQuery;
-	private SwingResultModel resultModel;
+
+	private final JTextPane queryPane = new JTextPane();
+
+	private final SwingResultModel resultModel = new SwingResultModel();
 
 	@Override
-	protected void initialiseOWLView() throws Exception {
+	protected void initialiseOWLView() {
 		initializeReasoner();
 		setLayout(new BorderLayout());
 		add(createCenterComponent(), BorderLayout.CENTER);
@@ -43,13 +41,9 @@ public class SparqlQueryView extends AbstractOWLViewComponent {
 	private JComponent createCenterComponent() {
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(0,1));
-		queryPane = new JTextPane();
 		queryPane.setText(reasoner.getSampleQuery());
 		panel.add(new JScrollPane(queryPane, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER));
-		resultModel = new SwingResultModel();
 		BasicOWLTable results = new BasicOWLTable(resultModel) {
-			private static final long serialVersionUID = 9143285439978520141L;
-
 			@Override
 			protected boolean isHeaderVisible() {
 				return true;
@@ -66,20 +60,16 @@ public class SparqlQueryView extends AbstractOWLViewComponent {
 	private JComponent createBottomComponent() {
 		JPanel panel = new JPanel();
 		panel.setLayout(new FlowLayout(FlowLayout.CENTER));
-		executeQuery = new JButton("Execute");
-		executeQuery.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-					String query = queryPane.getText();
-					SparqlResultSet result = reasoner.executeQuery(query);
-					resultModel.setResults(result);
-				}
-				catch (SparqlReasonerException ex) {
-					ErrorLogPanel.showErrorDialog(ex);
-					JOptionPane.showMessageDialog(getOWLWorkspace(), ex.getMessage() + "\nSee the logs for more information.");
-				}
+		JButton executeQuery = new JButton("Execute");
+		executeQuery.addActionListener(e -> {
+			try {
+				String query = queryPane.getText();
+				SparqlResultSet result = reasoner.executeQuery(query);
+				resultModel.setResults(result);
+			}
+			catch (SparqlReasonerException ex) {
+				ErrorLogPanel.showErrorDialog(ex);
+				JOptionPane.showMessageDialog(getOWLWorkspace(), ex.getMessage() + "\nSee the logs for more information.");
 			}
 		});
 		panel.add(executeQuery);
